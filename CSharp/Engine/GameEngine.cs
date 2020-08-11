20a8a8606592236a7a84bc55c7718c83a78340b4
Sour <sour@sour.codes>
2020-08-11 13:55:42 -0600

    Added interface IDisposable to class GameEngine

--- CSharp/Engine/GameEngine.cs
+++ CSharp/Engine/GameEngine.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 namespace Engine
 {
 	public abstract class GameEngine
+		: IDisposable
 	{
 		private GameEngineOptions _options;
 
@@ -127,5 +128,39 @@ namespace Engine
 			//	Informs the game loop to stop looping.
 			_cts.Cancel();
 		}
+
+		#region IDisposable
+
+		private bool disposedValue;
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects)
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+				// TODO: set large fields to null
+				disposedValue = true;
+			}
+		}
+
+		// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+		// ~GameEngine()
+		// {
+		//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		//     Dispose(disposing: false);
+		// }
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+
+		#endregion
 	}
 }
